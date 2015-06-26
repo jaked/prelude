86eb12befb3da2b8c7c3fa942a937b36fcb60c9e
Jake Donham <jdonham@twitter.com>
2015-06-26 13:13:35 -0700

    open-cgit

--- personal/init.el
+++ personal/init.el
@@ -1,1 +1,11 @@
+(defun open-cgit ()
+  "Open cgit.twitter.biz link to current file at current line"
+  (interactive)
+  (let* ((filename (buffer-file-name))
+         (git-root (directory-file-name (expand-file-name (vc-git-root filename))))
+         (path (substring filename (length git-root)))
+         (repo (file-name-nondirectory git-root))
+         (lineno (number-to-string (line-number-at-pos))))
+    (browse-url (concat "https://cgit.twitter.biz/" repo "/tree" path "#n" lineno))))
+
 (server-start)
