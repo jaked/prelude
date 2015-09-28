9d337f29340abc8e35e91c7be1443d803dc46272
Jake Donham <jdonham@twitter.com>
2015-09-28 13:47:01 -0700

    turn off trailing whitespace checking / removal

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -337,8 +337,8 @@ indent yanked text (with prefix arg don't indent)."
 
 ;; whitespace-mode config
 (require 'whitespace)
-(setq whitespace-line-column 80) ;; limit line length
-(setq whitespace-style '(face tabs empty trailing lines-tail))
+(setq whitespace-line-column 100) ;; limit line length
+(setq whitespace-style '(face tabs empty lines-tail))
 
 ;; saner regex syntax
 (require 're-builder)
